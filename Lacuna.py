081e11e543a11494223a95c3e0a28dccc3cf3240
SCMC <jaredbray56@gmail.com>
2022-02-27 12:42:11 -0600

    Lacuna.py

--- Lacuna.py
+++ Lacuna.py
@@ -1,5 +1,10 @@
-from tkinter import*
+# Lacuna Egress 2/27/2022
+from tkinter import* 
 
-Window = Tk()
 
-Window.mainloop()
+
+Window = Tk() # Declare window object
+Window.title('Lacuna Egress') # Dunno what THIS could POSSIBLY do
+Window.geometry('400x300') # 'X * Y' 
+
+Window.mainloop() # Holds window active ; KEEP OUTSIDE OF ALL CODE
